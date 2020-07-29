69086ac84fb5221724442c8ceb28c45f5062d8a4
tieulam <ttl.t954@gmail.com>
2020-07-28 22:29:42 -0600

    add checklist

---
+++ js/checklist.js
@@ -0,0 +1,16 @@
+const checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
+
+let lastChecked;
+function handleCheck(e) {
+  let inBetween;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) inBetween = !inBetween;
+      if (inBetween) checkbox.checked = true;
+    })
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
