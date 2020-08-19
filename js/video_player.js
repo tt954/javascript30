000738daf1bfcfce6de2cb8487ffd2868d5b84a6
tieulam <ttl.t954@gmail.com>
2020-08-19 16:30:29 -0600

    update video player

--- js/video_player.js
+++ js/video_player.js
@@ -4,15 +4,22 @@ const playBtn = document.querySelector('.player__button.toggle');
 let isPlaying = false;
 
 function togglePlay() {
+  const icon = document.createElement('i');
+
   if (!isPlaying) {
+    icon.classList.add('fas', 'fa-pause');
+    playBtn.removeChild(document.querySelector('.fa-play'));
+    playBtn.appendChild(icon);
     video.play();
-    isPlaying = true;
+    isPlaying = !isPlaying;
   } else {
+    icon.classList.add('fas', 'fa-play');
+    playBtn.removeChild(document.querySelector('.fa-pause'));
+    playBtn.appendChild(icon);
     video.pause();
-    isPlaying = false;
+    isPlaying = !isPlaying;
   }
 }
-
 
 
 playBtn.addEventListener('click', togglePlay);
